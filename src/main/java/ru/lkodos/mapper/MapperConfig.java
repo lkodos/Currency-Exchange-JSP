6a6f0086dafef02e8d34e0da1fa8b40ee3b56611
lkodos <tshagimardanov@gmail.com>
2025-11-16 17:51:47 +0300

    done GET exchangeRates

--- src/main/java/ru/lkodos/mapper/MapperConfig.java
+++ src/main/java/ru/lkodos/mapper/MapperConfig.java
@@ -25,12 +25,12 @@ public class MapperConfig {
     private static void fullExchangeRateToExchangeRateDtoMap() {
         modelMapper.typeMap(FullExchangeRate.class, ExchangeRateDto.class).addMappings(mapper -> {
             mapper.map(FullExchangeRate::getBaseCurrencyId, (dest, v) -> dest.getBaseCurrency().setId((Integer) v));
-            mapper.map(FullExchangeRate::getBaseCurrencyCode, (dest, v) -> dest.getBaseCurrency().setName((String) v));
-            mapper.map(FullExchangeRate::getBaseCurrencyName, (dest, v) -> dest.getBaseCurrency().setCode((String) v));
+            mapper.map(FullExchangeRate::getBaseCurrencyCode, (dest, v) -> dest.getBaseCurrency().setCode((String) v));
+            mapper.map(FullExchangeRate::getBaseCurrencyName, (dest, v) -> dest.getBaseCurrency().setName((String) v));
             mapper.map(FullExchangeRate::getBaseCurrencySign, (dest, v) -> dest.getBaseCurrency().setSign((String) v));
             mapper.map(FullExchangeRate::getTargetCurrencyId, (dest, v) -> dest.getTargetCurrency().setId((Integer) v));
-            mapper.map(FullExchangeRate::getTargetCurrencyCode, (dest, v) -> dest.getTargetCurrency().setName((String) v));
-            mapper.map(FullExchangeRate::getTargetCurrencyName, (dest, v) -> dest.getTargetCurrency().setCode((String) v));
+            mapper.map(FullExchangeRate::getTargetCurrencyCode, (dest, v) -> dest.getTargetCurrency().setCode((String) v));
+            mapper.map(FullExchangeRate::getTargetCurrencyName, (dest, v) -> dest.getTargetCurrency().setName((String) v));
             mapper.map(FullExchangeRate::getTargetCurrencySign, (dest, v) -> dest.getTargetCurrency().setSign((String) v));
             mapper.map(FullExchangeRate::getRate, ExchangeRateDto::setRate);
         });
